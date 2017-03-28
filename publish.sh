9dc5bebdc41d38005c6a72cdae27729810ac9b7a
Tom Akehurst <t.m.akehurst@googlemail.com>
2017-03-28 16:59:06 +0100

    Updated

--- publish.sh
+++ publish.sh
@@ -6,6 +6,8 @@ npm run raml
 bundle exec jekyll build
 
 pushd ../../mocklab-docs
+git reset --hard origin/gh-pages
+git pull
 cp -rf ../mocklab-mothership/docs/_site/* .
 git checkout CNAME
 git add -A
