28745a6b14f0a67a55c9e8b3e97ed33cca6effda
Tom Akehurst <t.m.akehurst@googlemail.com>
2017-12-07 19:42:06 +0000

    Updated

--- publish.sh
+++ publish.sh
@@ -4,12 +4,14 @@ set -euo pipefail
 
 npm run raml
 export MIXPANEL_ID=71b55ac57b47107ca89d12dc7c75cf00
-bundle exec jekyll build
+export ALGOLIA_API_KEY='61e953f848b8a1d05c3be4620a5286db'
+rbenv exec bundle exec jekyll algolia push
+rbenv exec bundle exec jekyll build
 
-pushd ../../mocklab-docs
+pushd ../mocklab-docs
 git reset --hard origin/gh-pages
 git pull
-cp -rf ../mocklab-mothership/docs/_site/* .
+cp -rf ../mocklab-docs-sources/_site/* .
 git checkout CNAME
 git add -A
 git commit -m "Updated"
